a264ed2e7c6fc0ca2c005e7c2bb67299d880651a
Èric Corbí <eric@llos.co>
2023-12-22 19:47:53 +0100

    first commit

---
+++ src/components/Test.vue
@@ -0,0 +1,38 @@
+<template>
+    <button @click="log">
+        hola! click component
+    </button>
+</template>
+
+<script setup>
+import { onMounted, onUnmounted } from 'vue';
+
+const log = () => {
+    console.log('clicking')
+}
+onMounted(() => {
+    console.log('mounted element');
+});
+
+onUnmounted(() => {
+    console.log('unmounted')
+})
+</script>
+
+<style scoped>
+button {
+    border: 1px solid white; 
+    background: transparent;
+    color: white;
+    border-radius: 10px;
+    display: inline-block;
+    font-size: 30px;
+    padding: 10px 0;
+    cursor: pointer;
+
+    &:hover {
+        background: white;
+        color: black;
+    }
+}
+</style>
